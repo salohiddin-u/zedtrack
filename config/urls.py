77a2a1968c507fe8e6388fb8f5209603054f8ba6
salohiddin-u <saloh.usmonali@gmail.com>
2025-12-08 12:19:54 +0500

    update

--- config/urls.py
+++ config/urls.py
@@ -5,15 +5,7 @@ from attendance_tracker.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', dashboard, name='dashboard'),
-    path('<str:a>-to-<str:b>/', dashboard, name='dashboard'),
-    path('<int:c>/', dashboard, name='dashboard'),
-    path('index/', index, name='index'),
-    path('courses/', include('courses.urls')),
-    path('teachers/', include('teachers.urls')),
-    path('students/', include('students.urls')),
-    path('marking-attendace/', include('attendance_tracker.urls')),
+    path('', DashboardView.as_view(), name='dashboard'),
     path('accounts/', include('allauth.urls')),
-    path('zedai/', zedia, name='zedai'),
 
 ]
