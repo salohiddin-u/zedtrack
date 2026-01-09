16b99221ecece432fe3d56b538f73857a1164e6e
salohiddin-u <saloh.usmonali@gmail.com>
2026-01-09 13:35:45 +0500

    Update urls.py

--- attendance_tracker/urls.py
+++ attendance_tracker/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', DashboardView.as_view(), name="dashboard"),
     path('mark-attendance/', MarkingAttendanceView.as_view(), name="marking-attendance"),
     path('mark-attendance/<int:a>/', MarkAttendanceView.as_view(), name="mark-attendance"),
-
+    path('mark-attendance/create/<int:course_id>/', attendance_create, name="attendance-create"),
 ]
