3d7de00a326c369b026415cf3b22c81b13e4b398
Salohiddin Usmonaliyev <saloh.usmonali@gmail.com>
2026-02-24 16:56:28 +0500

    app name has changed

--- mark_attendance/apps.py
+++ mark_attendance/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AttendanceTrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'attendance_tracker'
+    name = 'mark_attendance'
--- mark_attendance/urls.py
+++ mark_attendance/urls.py
@@ -4,7 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', DashboardView.as_view(), name="dashboard"),
-    path('mark-attendance/', MarkingAttendanceView.as_view(), name="marking-attendance"),
+    path('mark-attendance/', SelectCourse.as_view(), name="select-course"),
     path('mark-attendance/<int:a>/', MarkAttendanceView.as_view(), name="mark-attendance"),
     path('mark-attendance/create/<int:course_id>/', attendance_create, name="attendance-create"),
+    path('history/', HistoryView.as_view(), name="history"),
+    path('history/<str:start_date>/<str:end_date>/', HistoryView.as_view(), name="history"),
 ]
